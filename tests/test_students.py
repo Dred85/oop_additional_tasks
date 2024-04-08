01877ee8c2f293fa0452e601dc7e4d1f3de6f3ba
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-08 20:18:15 +0300

    add tests

---
+++ tests/test_students.py
@@ -0,0 +1,15 @@
+import pytest
+
+from lesson1.students import Student
+
+@pytest.fixture
+def student():
+    return Student("Алиса", "3 курс")
+
+
+def test_init1(student):
+    assert student.name == "Алиса"
+    assert student.course == "3 курс"
+
+
+
