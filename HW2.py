4634abc2305a2942d7707eea1ee9695db1a29d84
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-11 18:14:23 +0300

    HW2 end

--- HW2.py
+++ HW2.py
@@ -1,26 +1,10 @@
-class Product:
-    """
-    Класс для описания товара в магазине
-    """
-    name: str
-    description: str
-    price: str
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
-
 class Category:
     """
     Класс для категорий товара
     """
     name: str
     description: str
-    __products: list
+    products: list  # Приватный атрибут для списка товаров
 
     category_count = 0
     product_count = 0
@@ -33,13 +17,60 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(self.__products)
 
-    @property
-    def new_products(self):
-        return f'{self.__products["name"]}, {self.__products["price"]}, руб. Остаток: {self.__products["quantity"]} шт.'
-
-    @new_products.setter
-    def new_products(self, product):
+    def add_product(self, product):
+        """
+        Добавляет товар в категорию
+        """
         self.__products.append(product)
+
+    @property
+    def products(self):
+        """
+        Возвращает список товаров в категории
+        """
+
+        return "".join([f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n" for product in self.__products])
+
+
+class Product:
+    """
+    Класс для описания товара в магазине
+    """
+    name: str
+    description: str
+    price: float  # Приватный атрибут для цены
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product):
+        """
+        Фабричный метод для создания нового товара
+        """
+        name, description, price, quantity = product["name"], product["description"], product["price"], product["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """
+        Геттер для цены
+        """
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """
+        Сеттер для цены с проверкой
+        """
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
 
 
 data = [
@@ -98,12 +129,5 @@ Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.
 product_item = Product('Test', 'Test', 1000, 10)
 print(product_item.price)
 
-product_item.price = 800
+product_item.price = -100
 print(product_item.price)
-
-
-"""Ожидаемый ответ:
-
-1000
-800
-"""
