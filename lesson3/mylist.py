2e0fc57194dc55c3ea4b9bd09833fe68593fe28f
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-16 21:25:58 +0300

    add my_list

--- lesson3/mylist.py
+++ lesson3/mylist.py
@@ -11,10 +11,24 @@
 
 
 class MyList:
-    pass
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.data})"
+
+    def __str__(self):
+        return f"{self.data}"
+
+    def __len__(self):
+        return len(self.data)
+
+    def __add__(self, other):
+        my_list = self.data + other.data
+        return MyList(my_list)
 
 
-# код для проверки 
+# код для проверки
 my_list1 = MyList([1, 2, 3])
 print(repr(my_list1))  # MyList([1, 2, 3])
 print(str(my_list1))  # [1, 2, 3]
@@ -22,4 +36,4 @@ print(len(my_list1))  # 3
 
 my_list2 = MyList([4, 5, 6])
 my_list3 = my_list1 + my_list2
-print(my_list3)  # [1, 2, 3, 4, 5, 6]
+print(my_list3) # [1, 2, 3, 4, 5, 6]
