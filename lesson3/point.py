f347044e4ee5bd222708c6098d57b7a9d3b87ac2
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-18 20:11:40 +0300

    add Point

--- lesson3/point.py
+++ lesson3/point.py
@@ -9,14 +9,28 @@
 
 
 class Point:
-    pass
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}{self.x, self.y}"
+
+    def __str__(self):
+        return f"{self.x, self.y}"
+
+    def __add__(self, other):
+        """магический метод, который позволяет складывать точки и возвращать новую точку."""
+        return Point(self.x + other.x,  self.y + other.y)
 
 
 # код для проверки 
 point1 = Point(1, 2)
 print(repr(point1))  # Point(1, 2)
 print(str(point1))  # (1, 2)
-
+#
 point2 = Point(3, 4)
 point3 = point1 + point2
 print(point3)  # (4, 6)
+ # (4, 6)
