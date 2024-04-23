bb13aca61b5d6721680d0b4e333c2002096d2cfc
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-23 21:00:34 +0300

    add employee2

--- lesson4/employee_2.py
+++ lesson4/employee_2.py
@@ -10,11 +10,26 @@ class Employee:
     def __init__(self, pay):
         self.pay = pay
 
+    def __add__(self, other):
+        if isinstance(other, (int, float)):
+            return self.pay + other
+        elif isinstance(other, Employee):
+            return self.pay + other.pay
+        else:
+            raise TypeError("Складывать можно только числа и экземпляры класса Employee")
 
-class Client:
+
+class Client():
 
     def __init__(self, pay):
         self.pay = pay
+    def __add__(self, other):
+        if isinstance(other, (int, float)):
+            return self.pay + other
+        elif isinstance(other, Client):
+            return self.pay + other.pay
+        else:
+            raise TypeError("Складывать можно только числа и экземпляры класса Employee")
 
 
 class Developer(Employee):
@@ -24,12 +39,16 @@ class Developer(Employee):
 class Manager(Employee):
     pass
 
+
 # код для проверки
 users = [Employee(50000), Client(100000), Developer(50000), Manager(50000)]
 
 total_salary = 0
 for user in users:
+    # print(user.pay)
     total_salary = user + total_salary
+
+
 
 print(total_salary)
 # Вывод: 150000
