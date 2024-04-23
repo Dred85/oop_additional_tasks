d054d439f45afe525613882caf9818de12d64e6a
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-23 20:27:37 +0300

    add employee

--- lesson4/employee.py
+++ lesson4/employee.py
@@ -12,10 +12,24 @@
 
 
 class Employee:
-    pass
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def get_salary(self):
+        return self.salary
 
 
-# код для проверки 
+class Manager(Employee):
+    def __init__(self, name, salary, bonus):
+        super().__init__(name, salary)
+        self.bonus = bonus
+
+    def get_salary(self):
+        return self.salary + self.bonus
+
+
+# код для проверки
 employee = Employee("John", 5000)
 print(employee.get_salary())  # 5000
 
