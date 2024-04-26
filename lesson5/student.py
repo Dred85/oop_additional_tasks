dc7ea95992c90ddacb148295c6ac7054adde09d8
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-26 22:28:13 +0300

    fix student

--- lesson5/student.py
+++ lesson5/student.py
@@ -3,7 +3,7 @@
 
 - __slots__ = ('name', 'age', 'grades'): список атрибутов, доступных объекту.
 
-Напишите класс Course, представляющий курс, имеющий следующие атрибуты:
+Напишите класс Course, представляющий курс, имеющий следующие атрибуты:.
 
 - __slots__ = ('name', 'students'): список атрибутов, доступных объекту.
 """
@@ -32,7 +32,6 @@ student1.name = "John"
 student1.age = 20
 student1.grades = [90, 80, 85]
 
-
 student2 = Student()
 student2.name = "Jane"
 student2.age = 21
@@ -41,5 +40,5 @@ student2.grades = [95, 85, 90]
 course = Course()
 course.name = "Math"
 course.students = [student1, student2]
+# course.grades = 20
 
-print(student1.name)
