f897cbebc20cfeaba004c584d59a5c9eef23e18e
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-29 11:34:30 +0300

    add cars

--- lesson6/cars.py
+++ lesson6/cars.py
@@ -7,14 +7,25 @@
 
 Важно в конструкторе обрабатывать исключения, если год больше текущего
 """
+from datetime import date
 
+
+today = date.today()
 
 class Car:
-    pass
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        if year <= today.year:
+            self.year = year
+        else:
+            raise ValueError('Эта машина еще не была выпущена')
 
 
 # код для проверки
-car = Car('Toyota', 'Corolla', 2022)
+car1 = Car('Toyota', 'Corolla', 2022)
 
-car = Car('Toyota', 'Corolla', 3000)
+car2 = Car('Toyota', 'Corolla', 2025)
+print(car1.__dict__)
+print(car2.__dict__)
 # raises Exception('Эта машина еще не была выпущена')
