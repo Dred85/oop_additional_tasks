4cd53352af4f3bef1e03b41f486306bd9d609dfa
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-29 12:53:02 +0300

    add stusent

--- lesson6/student.py
+++ lesson6/student.py
@@ -12,13 +12,22 @@
 
 class Student:
 
-    def __init__(...):
-        ...
+    def __init__(self, name, course, marks):
+        self.name = name
+        self.course = course
+        self.marks = marks
+
+    def avg_rate(self):
+        try:
+            return sum(self.marks) / len(self.marks)
+        except ZeroDivisionError:
+            return 0.0
 
 
 # код для проверки
 student = Student('Ivan', 'Python', [5, 4, 5, 5])
-student.avg_rate() # 4.75
+print(student.avg_rate())
+
 
 student = Student('Ivan', 'Python', [])
-student.avg_rate() # 0.0
+print(student.avg_rate())
